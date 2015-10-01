43e96a306702316767b2218d427cd374dd0947a6
Tobias Brunner <tobias.brunner@vshn.ch>
2015-10-01 17:53:55 +0200

    enable ipv6

--- manifests/resources/site.pp
+++ manifests/resources/site.pp
@@ -131,6 +131,7 @@ define uhosting::resources::site (
   # vhost default parameters
   $vhost_global_defaults = {
     ensure               => $ensure,
+    ipv6_enable          => true,
     www_root             => $webroot,
     server_name          => $server_names,
     index_files          => ['index.html'],
@@ -142,8 +143,8 @@ define uhosting::resources::site (
     rewrite_to_https     => $rewrite_to_https,
     ssl_protocols        => 'TLSv1 TLSv1.1 TLSv1.2',
     ssl_ciphers          => 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA',
-    #ssl_stapling         => true, TODO needs more work: http://nginx.org/en/docs/http/ngx_http_ssl_module.html#ssl_stapling_verify
-    #ssl_stapling_verify  => true,
+    #ssl_stapling        => true, TODO needs more work: http://nginx.org/en/docs/http/ngx_http_ssl_module.html#ssl_stapling_verify
+    #ssl_stapling_verify => true,
     add_header           => $hsts,
   }
 
