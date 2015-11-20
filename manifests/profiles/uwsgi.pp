e12a76423c5113e0e3b32221c096cd307fa5062e
Tobias Brunner <tobias.brunner@vshn.ch>
2015-11-20 17:55:15 +0100

    use system uwsgi

--- manifests/profiles/uwsgi.pp
+++ manifests/profiles/uwsgi.pp
@@ -2,20 +2,21 @@
 class uhosting::profiles::uwsgi {
 
   # We run in Emperor mode!
-  apt::source { 'osso_ppa':
-    comment  => 'uwsgi osso PPA',
-    location => 'http://ppa.launchpad.net/osso/uwsgi/ubuntu',
-    release  => $::lsbdistcodename,
-    repos    => 'main',
-    key      => {
-      'id' => 'A8E0D05D50EC0EDA4958B44535CF57C9BD6901E2',
-      'server' => 'hkp://keyserver.ubuntu.com:80',
-    },
-    include  => {
-      'src' => false,
-      'deb' => true,
-    }
-  } ->
+  #Exec['apt_update'] -> Package <| |>
+  #apt::source { 'osso_ppa':
+  #  comment  => 'uwsgi osso PPA',
+  #  location => 'http://ppa.launchpad.net/osso/uwsgi/ubuntu',
+  #  release  => $::lsbdistcodename,
+  #  repos    => 'main',
+  #  key      => {
+  #    'id' => 'A8E0D05D50EC0EDA4958B44535CF57C9BD6901E2',
+  #    'server' => 'hkp://keyserver.ubuntu.com:80',
+  #  },
+  #  include  => {
+  #    'src' => false,
+  #    'deb' => true,
+  #  }
+  #} ->
   package { [
     'uwsgi-core',
     'uwsgi-emperor',
@@ -27,6 +28,11 @@ class uhosting::profiles::uwsgi {
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
+  } ->
+  file { '/etc/uwsgi-emperor/vassals':
+    ensure  => directory,
+    purge   => true,
+    recurse => true,
   } ~>
   service { 'uwsgi-emperor':
     ensure => running,
@@ -34,26 +40,17 @@ class uhosting::profiles::uwsgi {
   }
 
   # create directory for the uwsgi sockets
-  file { '/run/uwsgi':
-    ensure => directory,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0775',
-  }
+  #file { '/run/uwsgi':
+  #  ensure => directory,
+  #  owner  => 'www-data',
+  #  group  => 'www-data',
+  #  mode   => '0775',
+  #}
 
   # a world accessible spooler dir
   file { '/var/spool/uwsgi':
     ensure => directory,
     mode   => '0777',
   }
-
-  # manage vassals_dir
-  file { '/etc/uwsgi-emperor/vassals':
-    ensure  => directory,
-    purge   => true,
-    recurse => true,
-    require => Package['uwsgi-emperor'],
-  }
-
 
 }
