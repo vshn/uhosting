3a8f3e6af66695c15380aad3f43dcd07d28a6ab7
David Gubler <david.gubler@vshn.ch>
2016-01-15 15:08:10 +0100

    nodejs support: install nodejs and set up supervisord

---
+++ manifests/profiles/nodejs.pp
@@ -0,0 +1,25 @@
+# == Class: uhosting::profiles::nodejs
+#
+# Installs and manages NodeJS
+#
+# === Authors
+#
+# David Gubler <david.gubler@vshn.ch>
+#
+# === Copyright
+#
+# Copyright 2015 David Gubler, VSHN AG
+#
+class uhosting::profiles::nodejs {
+  # Make sure we always have the stable version installed as a default. This also provides the default npm.
+  # Note: All of these might be included multiple times. Only set things up once.
+  ensure_resource('nodejs::install', "nodejs-${::nodejs_stable_version}", { 'make_install' => false, 'version' => $::nodejs_stable_version })
+  ensure_resource('file', '/usr/local/bin/node', { 
+    'require' => Nodejs::Install[ "nodejs-${::nodejs_stable_version}" ],
+    'ensure'  => "/usr/local/node/node-${::nodejs_stable_version}/bin/node",
+  })
+  ensure_resource('file', '/usr/local/bin/npm', { 
+    'require' => File[ '/usr/local/bin/node' ],
+    'ensure' => "/usr/local/node/node-${::nodejs_stable_version}/bin/npm",
+  })
+}
