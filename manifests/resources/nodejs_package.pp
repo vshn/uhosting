765bdec87efa13f64affb09410507b9aa0683778
David Gubler <david.gubler@vshn.ch>
2016-01-15 17:39:42 +0100

    style fixes

--- manifests/resources/nodejs_package.pp
+++ manifests/resources/nodejs_package.pp
@@ -13,35 +13,37 @@
 # Copyright 2015 David Gubler, VSHN AG
 #
 define uhosting::resources::nodejs_package (
-  $ensure = present,
   $user,
   $homedir,
+  $ensure = present,
 ) {
   include uhosting::profiles::nodejs
+
   $_package = regsubst($title, "^${user}-", "")
   $_package_split = split($_package, '@')
   $_package_name = $_package_split[0]
   $_package_version = $_package_split[1]
   $validate = "/${_package_name}:${_package_name}@${_package_version}"
+
   if $ensure == present {
     exec { "npm_install_${user}_${_package_name}":
-      require     => File[ "/usr/local/bin/npm" ],
       command     => "npm install ${_package}",
-      unless      => "npm list -p -l | grep -q '${validate}'",
-      path        => $::path,
-      user        => $user,
-      environment => "HOME=${homedir}",
       cwd         => "${homedir}",
+      environment => "HOME=${homedir}",
+      path        => $::path,
+      unless      => "npm list -p -l | grep -q '${validate}'",
+      user        => $user,
+      require     => File[ "/usr/local/bin/npm" ],
     }
   } else {
     exec { "npm_remove_${user}_${_package_name}":
-      require     => File[ "/usr/local/bin/npm" ],
       command     => "npm remove ${_package_name}",
+      cwd         => "${homedir}",
+      environment => "HOME=${homedir}",
       onlyif      => "npm list -p -l | grep -q '${validate}'",
       path        => $::path,
       user        => $user,
-      environment => "HOME=${homedir}",
-      cwd         => "${homedir}",
+      require     => File[ "/usr/local/bin/npm" ],
     }
   }
 }
